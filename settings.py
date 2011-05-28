e2533e8a1cdde213d3749dcab2d4b87c6efbd70a
Mjumbe Wawatu Poe <mjumbe@codeforamerica.org>
2011-05-28 04:19:29 -0700

    Enable the messages framework

--- settings.py
+++ settings.py
@@ -96,6 +96,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
 )
 
 ROOT_URLCONF = 'philly_legislative.urls'
@@ -118,6 +119,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.admin',
+    'django.contrib.messages',
     'django.contrib.staticfiles',
 
     'south',
