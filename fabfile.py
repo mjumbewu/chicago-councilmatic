fa590b40a9e2f154ca996b2e5d7f727d5560b299
Mjumbe Wawatu Poe <mjumbe@codeforamerica.org>
2011-06-24 19:42:12 -0700

    Add a few comments tot he fab file.

--- fabfile.py
+++ fabfile.py
@@ -1,10 +1,12 @@
 from fabric.api import local
 
 def clean_compiled():
+    """Removed the compiled Python files"""
     local(r'rm -f `find . -name \*.pyc`')
     local(r'rm -f `find . -name \*.pyo`')
 
 def clean_temp():
+    """Clean up the temporary files"""
     local(r'rm -f `find . -name \*~`')
 
 def clean():
