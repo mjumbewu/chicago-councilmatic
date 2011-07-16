d538dadee5a69e4b2bd9b174c5ab9f9fcca92e72
Mjumbe Wawatu Poe <mjumbewu@gmail.com>
2011-07-16 09:38:54 -0700

    Add a page for the site main dashboard

--- councilmatic/main/views.py
+++ councilmatic/main/views.py
@@ -1,1 +1,21 @@
 # Create your views here.
+from django.views import generic as view
+
+import phillyleg.models
+import subscriptions.forms
+
+class AppDashboardView (view.TemplateView):
+
+    template_name = 'main/app_dashboard.html'
+    
+    def get_context_data(self, **kwargs):
+        legfiles = phillyleg.models.LegFile.objects.all().order_by('-key')[:8]
+        searchform = subscriptions.forms.SimpleSearchForm()
+        
+        context_data = super(AppDashboardView, self).get_context_data(
+            **kwargs)
+        context_data.update({'legfiles': legfiles, 
+                             'searchform': searchform})
+                       
+        return context_data
+            
--- councilmatic/urls.py
+++ councilmatic/urls.py
@@ -4,9 +4,10 @@ from django.conf.urls.defaults import *
 from django.contrib import admin
 admin.autodiscover()
 
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 import phillyleg.models
 
+import main.views
 import subscriptions.views
 import haystack.views
 
@@ -19,22 +20,35 @@ urlpatterns = patterns('',
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    (r'^admin/', admin.site.urls),
-
-    (r'^/$', 'phillyleg.views.index'),
-    (r'^subs/$', 'phillyleg.views.subscribe'),
-    (r'^subs/create/$', 'phillyleg.views.create'),
-    (r'^subs/unsubscribe/$', 'phillyleg.views.unsubscribe'),
-    #(r'^subs/(?P<subscription_id>\d+)/$', 'phillyleg.views.edit'),
-    (r'^subs/delete/$', 'phillyleg.views.delete'),
+    url(r'^admin/', admin.site.urls),
     
-    (r'^legislation/files', ListView.as_view(
+    url(r'^$', main.views.AppDashboardView.as_view(),
+        name='main_dashboard'),
+    
+    url(r'^legislation/$', ListView.as_view(
+        model=phillyleg.models.LegFile,
+        template_name='phillyleg/legfile_list.html',
+        paginate_by=20),
+        name='legislation_list'),
+    url(r'^legislation/(?P<pk>\d+)$', DetailView.as_view(
+        model=phillyleg.models.LegFile,
+        template_name='phillyleg/legfile_detail.html'),
+        name='legislation_detail'),
+
+    url(r'^subs/$', 'phillyleg.views.subscribe'),
+    url(r'^subs/create/$', 'phillyleg.views.create'),
+    url(r'^subs/unsubscribe/$', 'phillyleg.views.unsubscribe'),
+    #url(r'^subs/(?P<subscription_id>\d+)/$', 'phillyleg.views.edit'),
+    url(r'^subs/delete/$', 'phillyleg.views.delete'),
+    
+    url(r'^legislation/files', ListView.as_view(
         model=phillyleg.models.LegFile,
         template_name='legfile_list.html',
         context_object_name='legfile_list')),
     
-    (r'^search$', subscriptions.views.SearchView()),
-    (r'^subscribe$', subscriptions.views.SubscribeToSearchView.as_view()),
-    (r'^(?P<subscription_id>\d+)/$', 'phillyleg.views.dashboard'),
-#    (r'^search/', include('haystack.urls')),
+    url(r'^search$', subscriptions.views.SearchView(),
+        name='legfile_search'),
+    url(r'^subscribe$', subscriptions.views.SubscribeToSearchView.as_view()),
+    url(r'^(?P<subscription_id>\d+)/$', 'phillyleg.views.dashboard'),
+#    url(r'^search/', include('haystack.urls')),
 )
