285cac36108338e1661ffb62a47c7a8cbf831b09
Mjumbe Wawatu Poe <mjumbewu@gmail.com>
2011-07-16 23:32:41 -0700

    Add object_list context variable to search template.  Override the create_response method in SearchView.  This should not be necessary, but is since all of the variables in create_response are local, and I cannot get access to the page object from another function.  I need access to the page function so that I can pull out the search results objects.

--- councilmatic/subscriptions/views.py
+++ councilmatic/subscriptions/views.py
@@ -1,6 +1,8 @@
 from django.views import generic as views
 from django import http
 from haystack import views as haystack_views
+from django.shortcuts import render_to_response
+
 
 import subscriptions.forms as forms
 import subscriptions.models as models
@@ -14,15 +16,36 @@ class SearchView (haystack_views.SearchView):
     
     def extra_context(self):
         return { 'subs_form': self.get_embedded_subscribe_form() }
-
-
-class SubscribeToSearchView (SearchView, views.edit.ProcessFormView):
-    def get_subscription_form(self):
-        pass
     
-    def __call__(self, request):
-        if request.method == 'POST':
-            
-            subs_form = self.get_subscription_form()
-        else:
-            return super(SubscribeToSearchView, self).__call__(request)
+    def create_response(self):
+        """
+        Generates the actual HttpResponse to send back to the user.
+        """
+        (paginator, page) = self.build_page()
+        
+        context = {
+            'query': self.query,
+            'form': self.form,
+            'page': page,
+            'object_list': [result.object for result in page.object_list],
+            'paginator': paginator,
+            'suggestion': None,
+        }
+        
+        context.update(self.extra_context())
+        return render_to_response(self.template, context, context_instance=self.context_class(self.request))
+    
+
+class SubscribeToSearchView (views.CreateView):
+    model = models.SearchSubscription
+    template_name = "subscriptions/searchsubscription_edit.html"
+    
+#    def get_subscription_form(self):
+#        pass
+#    
+#    def __call__(self, request):
+#        if request.method == 'POST':
+#            
+#            subs_form = self.get_subscription_form()
+#        else:
+#            return super(SubscribeToSearchView, self).__call__(request)
