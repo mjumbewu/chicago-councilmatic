5534fb0d84c4dce8a194afba713973770627566f
Mjumbe Wawatu Poe <mjumbewu@gmail.com>
2011-08-04 23:31:49 -0700

    Added a headshot field for councilmembers.  This will hold a URL to their picture

--- councilmatic/phillyleg/models.py
+++ councilmatic/phillyleg/models.py
@@ -11,6 +11,7 @@ from django.core.urlresolvers import reverse
 
 class CouncilMember(models.Model):
     name = models.CharField(max_length=100)
+    headshot = models.URLField(null=True)
     
     def __unicode__(self):
         return self.name.lstrip("Councilmember")
