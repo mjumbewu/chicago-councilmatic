cccba064761ce89caddfea813552808533e740b5
Mjumbe Wawatu Poe <mjumbewu@gmail.com>
2011-08-07 14:24:03 -0700

    More updates to the design

---
+++ councilmatic/phillyleg/migrations/0026_auto__chg_field_councilmember_headshot.py
@@ -0,0 +1,146 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        
+        # Fill in any NULL fields
+        for councilmember in orm.CouncilMember.objects.all():
+            if not councilmember.headshot:
+                councilmember.headshot = 'phillyleg/noun_project_416.png'
+                councilmember.save()
+        
+        # Changing field 'CouncilMember.headshot'
+        db.alter_column('phillyleg_councilmember', 'headshot', self.gf('django.db.models.fields.CharField')(max_length=255))
+
+
+    def backwards(self, orm):
+        
+        # Changing field 'CouncilMember.headshot'
+        db.alter_column('phillyleg_councilmember', 'headshot', self.gf('django.db.models.fields.CharField')(max_length=255, null=True))
+
+
+    models = {
+        'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        'auth.permission': {
+            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'phillyleg.councilmember': {
+            'Meta': {'object_name': 'CouncilMember'},
+            'headshot': ('django.db.models.fields.CharField', [], {'default': "'phillyleg/noun_project_416.png'", 'max_length': '255'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'phillyleg.councilmembersubscription': {
+            'Meta': {'object_name': 'CouncilMemberSubscription'},
+            'councilmember': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['phillyleg.CouncilMember']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'subscription': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'councilmembers'", 'to': "orm['phillyleg.Subscription']"})
+        },
+        'phillyleg.keywordsubscription': {
+            'Meta': {'object_name': 'KeywordSubscription'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'keyword': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'subscription': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'keywords'", 'to': "orm['phillyleg.Subscription']"})
+        },
+        'phillyleg.legaction': {
+            'Meta': {'unique_together': "(('file', 'date_taken', 'description', 'notes'),)", 'object_name': 'LegAction'},
+            'acting_body': ('django.db.models.fields.CharField', [], {'max_length': '1000'}),
+            'date_taken': ('django.db.models.fields.DateTimeField', [], {}),
+            'description': ('django.db.models.fields.CharField', [], {'max_length': '1000'}),
+            'file': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['phillyleg.LegFile']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'minutes': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['phillyleg.LegMinutes']", 'null': 'True'}),
+            'motion': ('django.db.models.fields.CharField', [], {'max_length': '1000'}),
+            'notes': ('django.db.models.fields.TextField', [], {})
+        },
+        'phillyleg.legfile': {
+            'Meta': {'object_name': 'LegFile'},
+            'bookmarks': ('django.db.models.fields.related.ManyToManyField', [], {'related_name': "'bookmarks'", 'symmetrical': 'False', 'to': "orm['auth.User']"}),
+            'contact': ('django.db.models.fields.CharField', [], {'max_length': '1000'}),
+            'controlling_body': ('django.db.models.fields.CharField', [], {'max_length': '1000'}),
+            'date_scraped': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'final_date': ('django.db.models.fields.DateField', [], {'null': 'True'}),
+            'id': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True'}),
+            'intro_date': ('django.db.models.fields.DateField', [], {'null': 'True'}),
+            'key': ('django.db.models.fields.IntegerField', [], {'primary_key': 'True'}),
+            'last_scraped': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'sponsors': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['phillyleg.CouncilMember']", 'symmetrical': 'False'}),
+            'status': ('django.db.models.fields.CharField', [], {'max_length': '1000'}),
+            'title': ('django.db.models.fields.TextField', [], {}),
+            'type': ('django.db.models.fields.CharField', [], {'max_length': '1000'}),
+            'url': ('django.db.models.fields.CharField', [], {'max_length': '2048'}),
+            'version': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'phillyleg.legfileattachment': {
+            'Meta': {'unique_together': "(('file', 'url'),)", 'object_name': 'LegFileAttachment'},
+            'description': ('django.db.models.fields.CharField', [], {'max_length': '1000'}),
+            'file': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['phillyleg.LegFile']"}),
+            'fulltext': ('django.db.models.fields.TextField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'url': ('django.db.models.fields.CharField', [], {'max_length': '2048'})
+        },
+        'phillyleg.legfilemetadata': {
+            'Meta': {'object_name': 'LegFileMetaData'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'legfile': ('django.db.models.fields.related.OneToOneField', [], {'related_name': "'metadata'", 'unique': 'True', 'to': "orm['phillyleg.LegFile']"}),
+            'mentioned_legfiles': ('django.db.models.fields.related.ManyToManyField', [], {'related_name': "'references'", 'symmetrical': 'False', 'to': "orm['phillyleg.LegFile']"}),
+            'words': ('django.db.models.fields.related.ManyToManyField', [], {'related_name': "'references'", 'symmetrical': 'False', 'to': "orm['phillyleg.MetaData_Word']"})
+        },
+        'phillyleg.legminutes': {
+            'Meta': {'object_name': 'LegMinutes'},
+            'date_taken': ('django.db.models.fields.DateField', [], {'null': 'True'}),
+            'fulltext': ('django.db.models.fields.TextField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'url': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '2048'})
+        },
+        'phillyleg.metadata_word': {
+            'Meta': {'object_name': 'MetaData_Word'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'value': ('django.db.models.fields.CharField', [], {'max_length': '64'})
+        },
+        'phillyleg.subscription': {
+            'Meta': {'object_name': 'Subscription'},
+            'email': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'last_sent': ('django.db.models.fields.DateTimeField', [], {})
+        }
+    }
+
+    complete_apps = ['phillyleg']
