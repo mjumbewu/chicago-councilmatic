809c9aceefbbf3d1bdcea61b105b05055832c86c
Mjumbe Wawatu Poe <mjumbewu@gmail.com>
2011-09-07 04:42:02 -0700

    Add views for adding and removing bookmarks

--- councilmatic/bookmarks/views.py
+++ councilmatic/bookmarks/views.py
@@ -1,1 +1,60 @@
-# Create your views here.
+from django.contrib import messages
+from django.contrib import contenttypes
+from django.http import HttpResponseRedirect
+from django.views import generic as views
+
+import bookmarks.forms as forms
+import bookmarks.models as models
+
+
+class SingleBookmarkedObjectMixin (object):
+    def get_context_data(self, **kwargs):
+        context = super(SingleBookmarkedObjectMixin, self).get_context_data(**kwargs)
+
+        user = self.request.user
+        contenttype = contenttypes.models.ContentType.objects.get_for_model(self.object)
+
+        if not user.is_authenticated():
+            bookmark = None
+            is_bookmarked = False
+            bookmark_form = None
+        else:
+            try:
+                bookmark = user.bookmarks.get(content_id=self.object.pk,
+                                              content_type=contenttype.pk)
+                is_bookmarked = True
+                bookmark_form = None
+
+            except models.Bookmark.DoesNotExist:
+                bookmark = models.Bookmark(content_id=self.object.pk,
+                                           content_type=contenttype)
+                is_bookmarked = False
+                bookmark_form = forms.BookmarkForm({'user': self.request.user.pk,
+                                                    'content_id': self.object.pk,
+                                                    'content_type': contenttype.pk})
+
+        context['bookmark'] = bookmark
+        context['is_bookmarked'] = is_bookmarked
+        context['content_type'] = contenttype.pk
+        context['bookmark_form'] = bookmark_form
+
+        return context
+
+
+class CreateBookmarkView (views.CreateView):
+    form_class = forms.BookmarkForm
+
+    def form_invalid(self, form):
+        messages.add_message(request, messages.ERROR, 'Could not bookmark content')
+        messages.add_message(request, messages.DEBUG, form.errors)
+        return HttpResponseRedirect(self.request.POST['next'])
+
+    def get_success_url(self):
+        return self.request.POST['next']
+
+
+class DeleteBookmarkView (views.DeleteView):
+    model = models.Bookmark
+
+    def get_success_url(self):
+        return self.request.POST['next']
