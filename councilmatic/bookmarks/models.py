378b50fe9722c9a6697b20afaed0ff2c9ca679fe
Mjumbe Wawatu Poe <mjumbewu@gmail.com>
2011-09-07 04:41:16 -0700

    Add a model of bookmark

--- councilmatic/bookmarks/models.py
+++ councilmatic/bookmarks/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes import generic
 
-# Create your models here.
+class Bookmark (models.Model):
+    user = models.ForeignKey('auth.User', related_name='bookmarks')
+    content_id = models.PositiveIntegerField()
+    content_type = models.ForeignKey(ContentType)
+    content = generic.GenericForeignKey('content_id', 'content_type')
