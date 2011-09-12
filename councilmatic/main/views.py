9d177354975c56f01206f31bf0ddd54726fb2084
Mjumbe Wawatu Poe <mjumbewu@gmail.com>
2011-09-11 22:18:25 -0700

    Make content feed available in the legislation detail view

--- councilmatic/main/views.py
+++ councilmatic/main/views.py
@@ -1,7 +1,8 @@
-# Create your views here.
+import logging as log
 from django.views import generic as view
 
 from main import feeds
+import bookmarks.views
 import phillyleg.models
 import subscriptions.forms
 import subscriptions.models
@@ -10,7 +11,7 @@ import subscriptions.models
 class SearchBarMixin (object):
     def get_searchbar_form(self):
         return subscriptions.forms.SimpleSearchForm()
-    
+
     def get_context_data(self, **kwargs):
         context_data = super(SearchBarMixin, self).get_context_data(**kwargs)
         context_data.update({'searchbar_form': self.get_searchbar_form()})
@@ -18,32 +19,33 @@ class SearchBarMixin (object):
 
 
 class ContentFeedMixin (object):
+    feed_data = None
+    """A factory for the feed_data object that describes this content feed"""
+
     def get_content_feed(self, *args, **kwargs):
         feed_data = self.feed_data(*args, **kwargs)
         return subscriptions.models.ContentFeed.factory(feed_data)
-    
+
     def get_is_subscribed(self, feed):
-        if self.request.user:
+        if self.request.user and self.request.user.is_authenticated():
             try:
                 subscriber = self.request.user.subscriber
-            except AttributeError:
-                return False
+
+            # If the user doesn't have a subscriber attribute, then they must
+            # not be subscribed.
             except subscriptions.models.Subscriber.DoesNotExist:
                 return False
-            
-            for subscription in subscriber.subscriptions.all():
-                print feed.data, subscription.feed.data
-                if type(feed.data) == type(subscription.feed.data):
-                    return True
-        
+
+            return subscriber.is_subscribed(feed)
+
         return False
-    
+
     def get_context_data(self, **kwargs):
         context_data = super(ContentFeedMixin, self).get_context_data(**kwargs)
-        
+
         feed = self.get_content_feed()
         is_subscribed = self.get_is_subscribed(feed)
-        
+
         context_data.update({'feed': feed,
                              'is_subscribed': is_subscribed})
         return context_data
@@ -51,16 +53,16 @@ class ContentFeedMixin (object):
 
 class AppDashboardView (SearchBarMixin, view.TemplateView):
     template_name = 'main/app_dashboard.html'
-    
+
     def get_context_data(self, **kwargs):
         search_form = subscriptions.forms.FullSearchForm()
-        
+
         legfiles = phillyleg.models.LegFile.objects.all()[:8]
-        
+
         context_data = super(AppDashboardView, self).get_context_data(
             **kwargs)
         context_data.update({'legfiles': legfiles, 'search_form': search_form})
-                       
+
         return context_data
 
 
@@ -69,12 +71,28 @@ class LegislationListView (SearchBarMixin, ContentFeedMixin, view.ListView):
     template_name = 'phillyleg/legfile_list.html'
     paginate_by = 20
     feed_data = feeds.NewLegislationFeed
-    
+
     def get_queryset(self):
         queryset = super(LegislationListView, self).get_queryset()
         return queryset.order_by('-intro_date')
-    
 
-class LegislationDetailView (SearchBarMixin, view.DetailView):
+
+class LegislationDetailView (SearchBarMixin, ContentFeedMixin, bookmarks.views.SingleBookmarkedObjectMixin, view.DetailView):
     model = phillyleg.models.LegFile
     template_name = 'phillyleg/legfile_detail.html'
+    feed_data = None
+
+    def on_object_gotten(self, legfile):
+        # Construct the feed_data factory
+#        raise Exception(dir(logging))
+        log.debug('Construct the feed data factory for %r' % legfile)
+        if not self.feed_data:
+            self.feed_data = lambda: feeds.LegislationUpdatesFeed(pk=legfile.pk)
+
+    def get_object(self):
+        legfile = super(LegislationDetailView, self).get_object()
+
+        # Intercept the object
+        self.on_object_gotten(legfile)
+
+        return legfile
