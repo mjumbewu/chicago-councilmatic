1fc649ca7c066542227d558f21693a1abd6993da
Mjumbe Wawatu Poe <mjumbewu@gmail.com>
2011-10-06 01:55:17 -0700

    Remove unused feeds when updating feeds

--- councilmatic/subscriptions/management/commands/updatefeeds.py
+++ councilmatic/subscriptions/management/commands/updatefeeds.py
@@ -3,6 +3,7 @@ from django.core.management.base import BaseCommand, CommandError
 from subscriptions.feeds import import_all_feeds
 from subscriptions.feeds import ContentFeedRecordUpdater
 from subscriptions.models import ContentFeedRecord
+from subscriptions.models import Subscription
 
 
 class Command(BaseCommand):
@@ -14,6 +15,10 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         import_all_feeds()
+
+        # Get rid of unused records
+        used_record_ids = Subscription.objects.values('feed_record__id').distinct()
+        ContentFeedRecord.objects.exclude(id__in=used_record_ids).delete()
 
         records = self.get_records()
         updater = ContentFeedRecordUpdater()
