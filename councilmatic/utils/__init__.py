dd0f5d9eb684920cc23a5653d34351367d33e1e6
Mjumbe Wawatu Poe <mjumbewu@gmail.com>
2011-10-24 15:26:56 -0700

    Move the utils.py content to utils/__init__.py so that there aren't conflicts between the two.

---
+++ councilmatic/utils/__init__.py
@@ -0,0 +1,39 @@
+import os
+import tempfile
+
+# Adapted from Scraperwiki utils
+
+def pdftoxml(pdfdata):
+    """converts pdf file to xml file"""
+    pdffout = tempfile.NamedTemporaryFile(suffix='.pdf')
+    pdffout.write(pdfdata)
+    pdffout.flush()
+
+    xmlin = tempfile.NamedTemporaryFile(mode='r', suffix='.xml')
+    tmpxml = xmlin.name # "temph.xml"
+    cmd = '/usr/bin/pdftohtml -xml -nodrm -zoom 1.5 -enc UTF-8 -noframes "%s" "%s"' % (pdffout.name, os.path.splitext(tmpxml)[0])
+    cmd = cmd + " >/dev/null 2>&1" # can't turn off output, so throw away even stderr yeuch
+    os.system(cmd)
+
+    pdffout.close()
+    #xmlfin = open(tmpxml)
+    xmldata = xmlin.read()
+    xmlin.close()
+    return xmldata
+
+def pdftotxt(pdfdata):
+    """converts pdf file to txt file"""
+    pdffout = tempfile.NamedTemporaryFile(suffix='.pdf')
+    pdffout.write(pdfdata)
+    pdffout.flush()
+
+    txtin = tempfile.NamedTemporaryFile(mode='r', suffix='.txt')
+    tmptxt = txtin.name # "temph.xml"
+    cmd = '/usr/bin/pdftotext -enc UTF-8 -layout "%s" "%s"' % (pdffout.name, os.path.splitext(tmptxt)[0])
+    cmd = cmd + " >/dev/null 2>&1" # can't turn off output, so throw away even stderr yeuch
+    os.system(cmd)
+
+    pdffout.close()
+    txtdata = txtin.read()
+    txtin.close()
+    return txtdata
