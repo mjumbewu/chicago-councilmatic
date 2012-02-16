835273d8d5221482ddbf38d55334fe5876ab82d1
Mjumbe Wawatu Poe <mjumbewu@gmail.com>
2012-02-15 23:59:00 -0500

    Fix the issue where clicking on a search option would close the option list

--- councilmatic/main/static/main/dashboard_search.js
+++ councilmatic/main/static/main/dashboard_search.js
@@ -1,25 +1,24 @@
 (function () {
-    
+
     var setup_checkbox_list = function(field_id) {
         var $field_label
           , $field_list;
-        
-        $field_label = $('#' + field_id + ' label');
+
+        $field_label = $('#' + field_id + ' > label');
         $field_list = $('#' + field_id + ' ul');
-        
+
         $field_list.hide();
         $field_label.click(function() {
             $field_list.slideToggle();
         });
     };
-    
+
     var init = function() {
         setup_checkbox_list('div_id_statuses');
         setup_checkbox_list('div_id_controlling_bodies');
         setup_checkbox_list('div_id_file_types');
         setup_checkbox_list('div_id_sponsors');
     };
-    
+
     init();
 })();
-
