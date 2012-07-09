beed170a9ccab21719e15fab230cc432581ab7b6
Mjumbe Wawatu Poe <mjumbewu@gmail.com>
2012-07-09 19:37:59 -0400

    Start adding a subscription management view

--- councilmatic/main/views.py
+++ councilmatic/main/views.py
@@ -1,9 +1,11 @@
+import json
 import logging as log
 from django.contrib.syndication.views import Feed as DjangoFeed
 from django.shortcuts import get_object_or_404
 from django.views import generic as views
 from haystack.query import SearchQuerySet
 
+from cm_api.resources import SubscriberResource
 from main import feeds
 from main import forms
 
@@ -260,4 +262,14 @@ class BookmarkListView (SearchBarMixin,
 
 class SubscriptionManagementView (SearchBarMixin,
                                   views.TemplateView):
-    template_name = 'main/subscription_management.html'
+    template_name = 'cm/profile_admin.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(SubscriptionManagementView, self).get_context_data(**kwargs)
+
+        if self.request.user.is_authenticated() and self.request.user.subscriber:
+            subscriber_data = SubscriberResource().serialize(self.request.user.subscriber)
+            subscriber_data['logged_in'] = True
+            context['subscriber_data'] = json.dumps(subscriber_data)
+
+        return context
