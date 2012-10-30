13dc6b6ed31809530f5baf44253b27f2bcd0db32
Mjumbe Wawatu Poe <mjumbewu@gmail.com>
2012-10-29 22:35:52 -0400

    Add the start of a subscription manager

---
+++ councilmatic/subscriptions/templatetags/subscription_labels.py
@@ -0,0 +1,15 @@
+from django import template
+from subscriptions.feeds import ContentFeedLibrary
+
+register = template.Library()
+
+@register.filter
+def subscription_title(subscription):
+    library = ContentFeedLibrary()
+    
+    feed = library.get_feed(subscription.feed_record)
+    return feed.get_label()
+
+@register.filter
+def has_been_sent(subscription):
+    return bool(subscription.dispatches.count())
