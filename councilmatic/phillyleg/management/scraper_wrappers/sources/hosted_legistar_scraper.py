bbcd55bea6b7586bdb2c5393feadd8eeb5d915f4
Forest Gregg <fgregg@uchicago.edu>
2013-01-04 15:23:46 -0600

    first draft of scraper wrapper

--- councilmatic/phillyleg/management/scraper_wrappers/sources/hosted_legistar_scraper.py
+++ councilmatic/phillyleg/management/scraper_wrappers/sources/hosted_legistar_scraper.py
@@ -5,6 +5,7 @@ import re
 import urllib2
 import utils
 from bs4 import BeautifulSoup
+from collections import defaultdict
 
 from legistar.scraper import LegistarScraper
 from legistar.config import Config, DEFAULT_CONFIG
@@ -22,247 +23,49 @@ class HostedLegistarSiteWrapper (object):
     def __init__(self, options):
         self.scraper = LegistarScraper(options)
 
-    def get_legfile_url(self, key):
-        return self.root_url + 'detailreport/?key=' + str(key)
-
-    def urlopen(self, *args, **kwargs):
-        return urllib2.urlopen(*args, **kwargs)
-
-    def scrape_legis_file(self, key, soup):
+    def scrape_legis_file(self, key, summary):
         '''Extract a record from the given document (soup). The key is for the
            sake of record-keeping.  It is the key passed to the site URL.'''
 
-        span = soup.find('span', {'id':'lblFileNumberValue'})
-        lid = span.text
+        legislation_attrs, legislation_history = self.scraper.expandLegislationSummary(summary)
 
-        span = soup.find('span', {'id':'lblFileTypeValue'})
-        ltype = span.text
-
-        span = soup.find('span', {'id':'lblFileStatusValue'})
-        lstatus = span.text
-
-        span = soup.find('span', {'id':'lblTitleValue'})
-        ltitle = span.text
-
-        span = soup.find('span', {'id':'lblControllingBodyValue'})
-        lbody = span.text
-
-        span = soup.find('span', {'id':'lblIntroDateValue'})
-        lintro = span.text
-
-        span = soup.find('span', {'id':'lblFinalActionValue'})
-        lfinal = span.text
-
-        span = soup.find('span', {'id':'lblVersionValue'})
-        lversion = span.text
-
-        span = soup.find('span', {'id':'lblContactValue'})
-        lcontact = span.text
-
-        span = soup.find('span', {'id':'lblSponsorsValue'})
-        lsponsors = span.text
-
+        history_detail, votes = scraper.expandHistorySummary(history_summary)
+    
         record = {
             'key' : key,
-            'id' : lid,
-            'url' : self.get_legfile_url(key),
-            'type' : ltype.strip(),
-            'status' : lstatus.strip(),
-            'title' : ltitle.strip(),
-            'controlling_body' : lbody,
-            'intro_date' : self.convert_date(lintro),
-            'final_date' : self.convert_date(lfinal),
-            'version' : lversion,
-            'contact' : lcontact.strip(),
-            'sponsors' : lsponsors
+            'id' : summary['Record #'],
+            'url' : summary['URL']
+            'type' : summary['Type'],
+            'status' : summary['Status'],
+            'title' : summary['Title'],
+            'controlling_body' : legislation_attrs['Current Controlling Legislative Body'],
+            'intro_date' : self.convert_date(summary['Intro Date'],
+            'final_date' : self.convert_date(summary['Final Date'],
+            'version' : summary['Version'],
+            'contact' : None,
+            'sponsors' : legislation_attrs['Sponsors']
         }
 
-        attachments = self.scrape_legis_attachments(key, soup)
-        actions = self.scrape_legis_actions(key, soup)
-        minutes = self.collect_minutes(actions)
+        attachments = legislation_attrs['Attachments']
+
+        actions = []
+        for act in legislation_history :
+            act_details, act_votes = scraper.expandHistorySummary(act)
+            action = {
+                'key' : key,
+                'date_taken' : act['Date'],
+                'acting_body' : act['Action By']['label'],
+                'motion' : act['Result'],
+                'status' : act['Status']                                 
+            }                                     
+            actions.append(action)
+        
+
+        minutes = None
 
         log.info('Scraped legfile with key %r' % (key,))
         log.debug("%r %r %r %r" % (record, attachments, actions, minutes))
         return record, attachments, actions, minutes
-
-    def get_minutes_date(self, minutes_url):
-        date_match = re.search('_(\d{2})-(\d{2})-(\d{2})_', minutes_url)
-        if date_match:
-            date_taken = datetime.date(
-                year=int('20' + date_match.group(1)),
-                month=int(date_match.group(2)),
-                day=int(date_match.group(3)),
-            )
-        else:
-            date_taken = ''
-
-        return date_taken
-
-    def get_minutes_doc(self, minutes_url):
-        fulltext = self.extract_pdf_text(minutes_url)
-        date_taken = self.get_minutes_date(minutes_url)
-
-        minutes_doc = {
-            'url' : minutes_url,
-            'fulltext' : fulltext,
-            'date_taken' : date_taken,
-        }
-
-        return minutes_doc
-
-    def collect_minutes(self, actions):
-        """
-        Given a list of legislative actions, collect the minutes data attached
-        to those actions.
-        """
-
-        minutes = {}
-        for action in actions:
-            minutes_url = action['minutes_url']
-            if minutes_url.endswith('.pdf'):
-                minutes_doc = self.get_minutes_doc(minutes_url)
-                minutes[minutes_url] = minutes_doc
-
-        return minutes.values()
-
-    def scrape_legis_attachments(self, key, soup):
-        """
-        Given a beautiful soup representation of legislative file, return the
-        list of attachments.
-        """
-
-        attachments = []
-
-        attach_div = soup.find('div', {'id' : 'divAttachmentsValue'})
-        for cell in attach_div.findAll('a'):
-            url = cell['href']
-
-            if url.endswith('.pdf'):
-                fulltext = self.extract_pdf_text(url)
-            else:
-                fulltext = ''
-
-            attachment = {
-                'key' : key,
-                'description' : cell.text,
-                'url' : url,
-                'fulltext' : fulltext,
-            }
-            attachments.append(attachment)
-
-        return attachments
-
-    def scrape_legis_actions(self, key, soup):
-        """
-        Given a beautiful soup representation of a legislative file,
-        return the actions taken on the file.
-        """
-
-        def get_action_cell_text(cell):
-            cell_a = cell.find('a')
-            if cell_a:
-                return cell_a.text
-            else:
-                return cell.text
-
-        def get_action_cell_resource(cell):
-            cell_a = cell.find('a')
-            if cell_a:
-                return cell_a['href']
-            else:
-                return ''
-
-        notes = []
-        actions = []
-
-        action_div = soup.find('div', {'id': 'divScroll'})
-        action_rows = action_div.findAll('tr')
-
-        for action_row in action_rows:
-            cells = action_row.findAll('td')
-
-            if len(cells) == 2:
-                # Sometimes, there are notes interspersed in the history table.
-                # Luckily (?) their rows have only two cells instead of four, so
-                # we can easily tell that they're there.
-                if actions:
-                    action = actions[-1]
-                    action['notes'] = cells[1].text.strip()
-                continue
-
-            action = {
-                'key' : key,
-                'date_taken' : self.convert_date(get_action_cell_text(cells[0])),
-                'acting_body' : get_action_cell_text(cells[1]).strip(),
-                'description' : get_action_cell_text(cells[2]).strip(),
-                'motion' : get_action_cell_text(cells[3]).strip(),
-                'minutes_url' : get_action_cell_resource(cells[0]).strip(),
-                'notes' : '',
-            }
-            actions.append(action)
-
-        return actions
-
-    __pdf_cache = None
-    def init_pdf_cache(self, seed=None):
-        if seed is None:
-            seed = {}
-        self.__pdf_cache = self.__pdf_cache or seed
-
-    def extract_pdf_text(self, pdf_data, tries_left=5):
-        """
-        Given an http[s] URL, a file URL, or a file-like object containing
-        PDF data, return the text from the PDF.  Cache URLs or data that have
-        already been seen.
-        """
-
-        self.init_pdf_cache()
-
-        pdf_key = pdf_data
-        if pdf_key in self.__pdf_cache:
-            pdf_content = unicode(self.__pdf_cache[pdf_key])
-            if pdf_content not in [None, 'None']:
-                return pdf_content
-
-        if pdf_key.startswith('file://'):
-            path = pdf_key[7:]
-            pdf_data = open(path).read()
-        elif pdf_key.startswith('http://') or pdf_key.startswith('https://'):
-            url = pdf_key
-            try:
-                pdf_data = self.urlopen(url).read()
-
-            # Protect against removed PDFs (ones that result in 404 HTTP
-            # response code).  I don't know why they've removed some PDFs
-            # but they have.
-            except urllib2.HTTPError, err:
-                if err.code == 404:
-                    self.__pdf_cache[pdf_key] = ''
-                    return ''
-                else:
-                    raise
-
-            # Been getting timeout exceptions every so often, so try again
-            # if timed out.
-            except urllib2.URLError, err:
-                if tries_left:
-                    return self.extract_pdf_text(pdf_key, tries_left-1)
-
-        xml_data = utils.pdftoxml(pdf_data)
-
-        self.__pdf_cache[pdf_key] = self.extract_xml_text(xml_data, 'pdf2xml')
-        return self.__pdf_cache[pdf_key]
-
-    def extract_xml_text(self, xml_data, root_node_name):
-        soup = BeautifulSoup(xml_data)
-        root_node = soup.find(root_node_name)
-
-        if root_node:
-            xml_text = root_node.text
-            return xml_text
-        # Some PDFs are images
-        else:
-            return ''
 
     def convert_date(self, orig_date):
         if orig_date:
@@ -276,12 +79,13 @@ class HostedLegistarSiteWrapper (object):
            paramter; just starts at the beginning for each instance of the
            scraper.
         '''
+        next_summary = defaultdict(str)
 
         if self.legislation_summaries is None:
             self.legislation_summaries =  self.scraper.searchLegislation('')
 
         try:
-            next_summary = self.legislation_summaries.next()
+            next_summary.update(self.legislation_summaries.next())
             return 0, next_summary
         except StopIteration:
             return None, None
