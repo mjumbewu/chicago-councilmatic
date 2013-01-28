1f941c37a9b90e1f2447878c3a479d3e4f102e70
Forest Gregg <fgregg@uchicago.edu>
2013-01-28 09:35:03 -0600

    fixed typo

--- councilmatic/phillyleg/management/scraper_wrappers/sources/hosted_legistar_scraper.py
+++ councilmatic/phillyleg/management/scraper_wrappers/sources/hosted_legistar_scraper.py
@@ -91,7 +91,7 @@ class HostedLegistarSiteWrapper (object):
         except StopIteration:
             return None, None
 
-    def init_pdf_cache(ds.pdf_mapping) :
+    def init_pdf_cache(pdf_mapping) :
         pass
         
     
