4e12f27e10d1f6a300f0c5a4ad67ff21dc487992
Forest Gregg <fgregg@uchicago.edu>
2013-03-24 08:34:38 -0500

    removed helping arguments for initial scrape

--- councilmatic/phillyleg/management/scraper_wrappers/sources/hosted_legistar_scraper.py
+++ councilmatic/phillyleg/management/scraper_wrappers/sources/hosted_legistar_scraper.py
@@ -26,7 +26,7 @@ class HostedLegistarSiteWrapper (object):
 
     def __init__(self, **options):
         self.scraper = LegistarScraper(options)
-        self.legislation_summaries =  self.scraper.searchLegislation('', created_before='2011-01-15') # hack for initial import of data
+        self.legislation_summaries =  self.scraper.searchLegislation('')
 
     def scrape_legis_file(self, key, summary):
         '''Extract a record from the given document (soup). The key is for the
