db2fac800fb8fbe03955531e943b766b90c94314
root <root@ip-10-202-250-75.ec2.internal>
2013-03-28 22:40:49 -0500

    set up caching on home page, updates to crons

--- councilmatic/settings.py
+++ councilmatic/settings.py
@@ -256,6 +256,13 @@ DEBUG_TOOLBAR_CONFIG = {
 }
 INTERNAL_IPS = ('127.0.0.1',)
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION': '127.0.0.1:11211',
+    }
+}
+
 # Logging
 LOGGING = {
     'version': 1,
