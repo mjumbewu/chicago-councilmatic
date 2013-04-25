3335b335c0fbf8a3576b4c2c6d75664ed3df69f7
root <root@ip-10-202-250-75.ec2.internal>
2013-04-25 18:00:43 -0500

    added metadata_topic migration

---
+++ councilmatic/phillyleg/migrations/0043_auto__add_metadata_topic.py
@@ -0,0 +1,168 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'MetaData_Topic'
+        db.create_table('phillyleg_metadata_topic', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('topic', self.gf('django.db.models.fields.CharField')(unique=True, max_length=128)),
+        ))
+        db.send_create_signal('phillyleg', ['MetaData_Topic'])
+
+        # Adding M2M table for field topics on 'LegFileMetaData'
+        db.create_table('phillyleg_legfilemetadata_topics', (
+            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
+            ('legfilemetadata', models.ForeignKey(orm['phillyleg.legfilemetadata'], null=False)),
+            ('metadata_topic', models.ForeignKey(orm['phillyleg.metadata_topic'], null=False))
+        ))
+        db.create_unique('phillyleg_legfilemetadata_topics', ['legfilemetadata_id', 'metadata_topic_id'])
+
+    def backwards(self, orm):
+        # Deleting model 'MetaData_Topic'
+        db.delete_table('phillyleg_metadata_topic')
+
+        # Removing M2M table for field topics on 'LegFileMetaData'
+        db.delete_table('phillyleg_legfilemetadata_topics')
+
+    models = {
+        'phillyleg.councildistrict': {
+            'Meta': {'object_name': 'CouncilDistrict'},
+            'created_datetime': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.IntegerField', [], {}),
+            'key': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'plan': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'districts'", 'to': "orm['phillyleg.CouncilDistrictPlan']"}),
+            'shape': ('django.contrib.gis.db.models.fields.PolygonField', [], {}),
+            'updated_datetime': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'})
+        },
+        'phillyleg.councildistrictplan': {
+            'Meta': {'object_name': 'CouncilDistrictPlan'},
+            'created_datetime': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'date': ('django.db.models.fields.DateField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'updated_datetime': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'})
+        },
+        'phillyleg.councilmember': {
+            'Meta': {'object_name': 'CouncilMember'},
+            'created_datetime': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'districts': ('django.db.models.fields.related.ManyToManyField', [], {'related_name': "'representatives'", 'symmetrical': 'False', 'through': "orm['phillyleg.CouncilMemberTenure']", 'to': "orm['phillyleg.CouncilDistrict']"}),
+            'headshot': ('django.db.models.fields.CharField', [], {'default': "'phillyleg/noun_project_416.png'", 'max_length': '255'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'updated_datetime': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'})
+        },
+        'phillyleg.councilmembertenure': {
+            'Meta': {'ordering': "('-begin',)", 'object_name': 'CouncilMemberTenure'},
+            'at_large': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'begin': ('django.db.models.fields.DateField', [], {'blank': 'True'}),
+            'councilmember': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'tenures'", 'to': "orm['phillyleg.CouncilMember']"}),
+            'created_datetime': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'district': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'tenures'", 'null': 'True', 'to': "orm['phillyleg.CouncilDistrict']"}),
+            'end': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'president': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'updated_datetime': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'})
+        },
+        'phillyleg.legaction': {
+            'Meta': {'ordering': "['date_taken']", 'unique_together': "(('file', 'date_taken', 'description', 'notes'),)", 'object_name': 'LegAction'},
+            'acting_body': ('django.db.models.fields.CharField', [], {'max_length': '1000'}),
+            'created_datetime': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'date_taken': ('django.db.models.fields.DateField', [], {}),
+            'description': ('django.db.models.fields.TextField', [], {}),
+            'file': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'actions'", 'to': "orm['phillyleg.LegFile']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'minutes': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'actions'", 'null': 'True', 'to': "orm['phillyleg.LegMinutes']"}),
+            'motion': ('django.db.models.fields.CharField', [], {'max_length': '1000'}),
+            'notes': ('django.db.models.fields.TextField', [], {}),
+            'updated_datetime': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'})
+        },
+        'phillyleg.legfile': {
+            'Meta': {'ordering': "['-key']", 'object_name': 'LegFile'},
+            'contact': ('django.db.models.fields.CharField', [], {'default': "'No contact'", 'max_length': '1000'}),
+            'controlling_body': ('django.db.models.fields.CharField', [], {'max_length': '1000'}),
+            'created_datetime': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'date_scraped': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'null': 'True', 'blank': 'True'}),
+            'final_date': ('django.db.models.fields.DateField', [], {'null': 'True'}),
+            'id': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True'}),
+            'intro_date': ('django.db.models.fields.DateField', [], {'default': 'datetime.datetime.now'}),
+            'key': ('django.db.models.fields.IntegerField', [], {'primary_key': 'True'}),
+            'last_scraped': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'sponsors': ('django.db.models.fields.related.ManyToManyField', [], {'related_name': "'legislation'", 'symmetrical': 'False', 'to': "orm['phillyleg.CouncilMember']"}),
+            'status': ('django.db.models.fields.CharField', [], {'max_length': '1000'}),
+            'title': ('django.db.models.fields.TextField', [], {}),
+            'type': ('django.db.models.fields.CharField', [], {'max_length': '1000'}),
+            'updated_datetime': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'url': ('django.db.models.fields.URLField', [], {'max_length': '200'}),
+            'version': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'phillyleg.legfileattachment': {
+            'Meta': {'unique_together': "(('file', 'url'),)", 'object_name': 'LegFileAttachment'},
+            'created_datetime': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'description': ('django.db.models.fields.CharField', [], {'max_length': '1000'}),
+            'file': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'attachments'", 'to': "orm['phillyleg.LegFile']"}),
+            'fulltext': ('django.db.models.fields.TextField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'updated_datetime': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'url': ('django.db.models.fields.URLField', [], {'max_length': '200'})
+        },
+        'phillyleg.legfilemetadata': {
+            'Meta': {'object_name': 'LegFileMetaData'},
+            'created_datetime': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'legfile': ('django.db.models.fields.related.OneToOneField', [], {'related_name': "'metadata'", 'unique': 'True', 'to': "orm['phillyleg.LegFile']"}),
+            'locations': ('django.db.models.fields.related.ManyToManyField', [], {'related_name': "'references_in_legislation'", 'symmetrical': 'False', 'to': "orm['phillyleg.MetaData_Location']"}),
+            'mentioned_legfiles': ('django.db.models.fields.related.ManyToManyField', [], {'related_name': "'references_in_legislation'", 'symmetrical': 'False', 'to': "orm['phillyleg.LegFile']"}),
+            'topics': ('django.db.models.fields.related.ManyToManyField', [], {'related_name': "'references_in_legislation'", 'symmetrical': 'False', 'to': "orm['phillyleg.MetaData_Topic']"}),
+            'updated_datetime': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'words': ('django.db.models.fields.related.ManyToManyField', [], {'related_name': "'references_in_legislation'", 'symmetrical': 'False', 'to': "orm['phillyleg.MetaData_Word']"})
+        },
+        'phillyleg.legkeys': {
+            'Meta': {'object_name': 'LegKeys'},
+            'continuation_key': ('django.db.models.fields.IntegerField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'})
+        },
+        'phillyleg.legminutes': {
+            'Meta': {'object_name': 'LegMinutes'},
+            'created_datetime': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'date_taken': ('django.db.models.fields.DateField', [], {'null': 'True'}),
+            'fulltext': ('django.db.models.fields.TextField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'updated_datetime': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'url': ('django.db.models.fields.URLField', [], {'unique': 'True', 'max_length': '200'})
+        },
+        'phillyleg.legminutesmetadata': {
+            'Meta': {'object_name': 'LegMinutesMetaData'},
+            'created_datetime': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'legminutes': ('django.db.models.fields.related.OneToOneField', [], {'related_name': "'metadata'", 'unique': 'True', 'to': "orm['phillyleg.LegMinutes']"}),
+            'locations': ('django.db.models.fields.related.ManyToManyField', [], {'related_name': "'references_in_minutes'", 'symmetrical': 'False', 'to': "orm['phillyleg.MetaData_Location']"}),
+            'updated_datetime': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'words': ('django.db.models.fields.related.ManyToManyField', [], {'related_name': "'references_in_minutes'", 'symmetrical': 'False', 'to': "orm['phillyleg.MetaData_Word']"})
+        },
+        'phillyleg.metadata_location': {
+            'Meta': {'object_name': 'MetaData_Location'},
+            'address': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '2048'}),
+            'created_datetime': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'geom': ('django.contrib.gis.db.models.fields.PointField', [], {'null': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'updated_datetime': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'valid': ('django.db.models.fields.BooleanField', [], {'default': 'True'})
+        },
+        'phillyleg.metadata_topic': {
+            'Meta': {'object_name': 'MetaData_Topic'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'topic': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '128'})
+        },
+        'phillyleg.metadata_word': {
+            'Meta': {'object_name': 'MetaData_Word'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'value': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '64'})
+        }
+    }
+
+    complete_apps = ['phillyleg']
