b7ea9cf1db98d03b828da430acc496b45b86eb5d
root <root@ip-10-202-250-75.ec2.internal>
2013-05-03 12:13:09 -0500

    added script to manually remove philly councilmembers, relates to #22

---
+++ ci/remove_philly_councilmembers.sql
@@ -0,0 +1,3 @@
+delete from phillyleg_legfile_sponsors where councilmember_id <= 37;
+delete from phillyleg_councilmembertenure where councilmember_id <= 37;
+delete from phillyleg_councilmember where id <= 37;
