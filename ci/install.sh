804c70a8c8e7aac98934b0f23259664ef8b8e9af
Forest Gregg <fgregg@uchicago.edu>
2013-06-06 11:55:02 -0500

    Update install.sh

--- ci/install.sh
+++ ci/install.sh
@@ -1,33 +1,31 @@
 #!/bin/sh
 
 # libevent development files are required for gevent
-sudo apt-get install libevent-dev
+apt-get install libevent-dev
 
 # Install GeoDjango dependencies -- see
 # https://docs.djangoproject.com/en/dev/ref/contrib/gis/install/#ubuntu
 apt-get install binutils gdal-bin libproj-dev postgresql-9.1-postgis \
         postgresql-server-dev-9.1 python-psycopg2
 
+virtualenv .env
+source .env/bin/activate
+
 echo "Install the python requirements"
 pip install -r requirements.txt
-
-echo "Install the non-python requirements"
-apt-get install poppler-utils
 
 echo "... and this, optional testing stuff"
 pip install coverage
 
-# Create a PostGIS template database
-psql -c "CREATE DATABASE template_postgis;" -U postgres
-psql -c "UPDATE pg_database SET datistemplate='true' WHERE datname='template_postgis';" -U postgres
-createlang plpgsql template_postgis -U postgres
-# Loading the PostGIS SQL routines
-psql -d template_postgis -f /usr/share/postgresql/9.1/contrib/postgis-1.5/postgis.sql -q
-psql -d template_postgis -f /usr/share/postgresql/9.1/contrib/postgis-1.5/spatial_ref_sys.sql -q
-# Enabling users to alter spatial tables.
-psql -d template_postgis -c "GRANT ALL ON geometry_columns TO PUBLIC;"
-psql -d template_postgis -c "GRANT ALL ON geography_columns TO PUBLIC;"
+
+psql -d postgres -c "UPDATE pg_database SET datistemplate='true' WHERE datname='template_postgis';"
+POSTGIS_SQL_PATH=`pg_config --sharedir`/contrib/postgis-1.5
+psql -d template_postgis -f $POSTGIS_SQL_PATH/postgis.sql # Loading the PostGIS SQL routines
+psql -d template_postgis -f $POSTGIS_SQL_PATH/spatial_ref_sys.sql
+psql -d template_postgis -c "GRANT ALL ON geometry_columns TO PUBLIC;" # Enabling users to alter spatial tables.
 psql -d template_postgis -c "GRANT ALL ON spatial_ref_sys TO PUBLIC;"
+
+createdb -T template_postgis councilmatic
 
 echo "Create the database"
 psql -U postgres <<EOF
@@ -36,7 +34,8 @@ psql -U postgres <<EOF
     GRANT ALL ON DATABASE template_postgis TO councilmatic;
     GRANT ALL ON DATABASE councilmatic TO councilmatic;
     ALTER USER councilmatic WITH CREATEDB;
-EOF
 
-echo "Initialize the project settings"
-cp councilmatic/local_settings.py.template councilmatic/local_settings.py
+exit #to return to your normal user
+----------------------------------- 
+
+cp councilmatic/oakland_local_settings.py.template councilmatic/local_settings.py
